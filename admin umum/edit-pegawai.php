7009f45c67870469f0fd02e7d4864ba03809b35b
risky maulana <lukeagnis7@gmail.com>
2024-12-30 03:12:57 +0900

    membuat tampilan yang disukai pak ardi:)

--- admin umum/edit-pegawai.php
+++ admin umum/edit-pegawai.php
@@ -5,10 +5,6 @@ include "layout/side.php";
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
-                        <h1 class="mt-4">Admin</h1>
-                        <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item active">Umum</li>
-                        </ol>
                         <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-edit"></i>
